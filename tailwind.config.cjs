8a645e5f9ed20ab93e6bfeea1998e43b731355f3
ivanfl1827 <ivanfl1827@gmail.com>
2025-10-26 13:27:10 +0000

    Changed Fixed

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
-  mode: "jit",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
